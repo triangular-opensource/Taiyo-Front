80fa2143ddde2f9463ed3c4472be6b3589321871
savagecarol <karthiksharma1411@gmail.com>
2021-12-07 10:58:08 +0530

    about page completed

--- src/components/About/AboutData/AboutData.js
+++ src/components/About/AboutData/AboutData.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import useAuth from '../../../config/AuthContext'
+import React from 'react';
+import useAuth from '../../../config/AuthContext';
+import './AboutData.css';
+import { ReactComponent as AboutSvg } from "../../../static/about.svg";
+import CustomText from "../../../customComponents/CustomText/CustomText";
+
 
 const AboutData = () => {
-
     const {generalInfo} = useAuth();
-
     return (
-        <div>
-            {generalInfo.about}
+        <div className = 'About__Container'>
+            <div className = 'About__Container__Left'>
+                 <CustomText name = {generalInfo.about} color = 'grey' size = 'large' weight = '400'/>               
+            </div>
+            <div className = 'About__Container__Right'>
+                 <AboutSvg/ >
+            </div>
         </div>
     )
 }
