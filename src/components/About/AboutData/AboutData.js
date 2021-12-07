a067d3183b6e5015cecc5e3d0ff218fbbf83a20a
Alankar Saxena <saxenaalankar42@gmail.com>
2021-12-07 15:07:49 +0530

    about page styling

--- src/components/About/AboutData/AboutData.js
+++ src/components/About/AboutData/AboutData.js
@@ -8,12 +8,14 @@ import CustomText from "../../../customComponents/CustomText/CustomText";
 const AboutData = () => {
     const {generalInfo} = useAuth();
     return (
-        <div className = 'About__Container'>
-            <div className = 'About__Container__Left'>
-                 <CustomText name = {generalInfo.about} color = 'grey' size = 'large' weight = '400'/>               
-            </div>
-            <div className = 'About__Container__Right'>
-                 <AboutSvg/ >
+        <div className='container About__Container py-5 my-5'>
+            <div className="row">
+                <div className='col-md-6 About__Container__Right py-2 mb-5'>
+                    <AboutSvg />
+                </div>
+                <div className='col-md-6 About__Container__Left pt-2'>
+                    <CustomText name={generalInfo.about} color='grey' size='large' weight='400'/>               
+                </div>
             </div>
         </div>
     )
