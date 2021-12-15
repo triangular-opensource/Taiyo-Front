599fa6b1822a48f164d217be08081b464705e31c
savagecarol <karthiksharma1411@gmail.com>
2021-12-14 23:33:23 -0800

    loading

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -14,13 +14,7 @@ function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-<<<<<<< HEAD
-=======
-    const [isEmailValidate, setEmailValidate] = useState(false);
-    const [isPasswordValidate, setPasswordValidate] = useState(false);
-    const [loading, setLoading] = useState(false);
-
->>>>>>> 174546e3b286f8ff203e819cedc51ef05706146a
+    const [loading , setLoading] = useState(false);
 
     const {login} = useAuth();
     const handleLogin = () => {
@@ -41,25 +35,9 @@ function Login() {
                         </legend>
                     </div>
                     <div className="form-row col-md-12">
-<<<<<<< HEAD
                         <CustomInput type="email" value={email}
                          onChangeValue = {(event) => setEmail(event.target.value)} placeholder='Email'  />
                         {(email!=="" && !emailValidation(email) ) ?  <CustomAlert  message = {EMAIL_ERROR}/ > : null }      
-=======
-                        <CustomInput
-                            type="email"
-                            value={email}
-                            onChangeValue = {(event) => { 
-                                setEmail(event.target.value)
-                                if(emailValidation(event.target.value))
-                                    setEmailValidate(true)
-                                else
-                                    setEmailValidate(false)
-                            }}
-                            placeholder='Email'
-                        />
-                        {!isEmailValidate && <CustomAlert message={EMAIL_ERROR} /> }      
->>>>>>> 174546e3b286f8ff203e819cedc51ef05706146a
                     </div>
                     <div className="form-row col-md-12">
                         <CustomInput
@@ -68,42 +46,11 @@ function Login() {
                             placeholder='Password'
                             onChangeValue = {(event) => { 
                                 setPassword(event.target.value)
-<<<<<<< HEAD
                             }} />
                            {(password!=="" && !passwordValidate(password)) ?  <PASSWORD_ERROR/> : null }        
                     </div>
                     <div className="form-row col-md-12">
                         <CustomButton  disabled = { ! ( (email!=="" && emailValidation(email)) && (password!=="" && passwordValidate(password)) ) } fontSize="17" marginTop="20" data="LOGIN" handleClick={handleLogin} padding='16' backgroundColor='gray' color='white' />
-=======
-                                if(passwordValidate(event.target.value))
-                                     setPasswordValidate(true)
-                                else
-                                    setPasswordValidate(false)  
-                            }}
-                        />
-                        { !isPasswordValidate &&  <PASSWORD_ERROR/> }        
-                    </div>
-                    <div className="form-row col-md-12">
-                        <CustomButton
-                            disabled={(isEmailValidate && isPasswordValidate)}
-                            fontSize="17"
-                            marginTop="20"
-                            data={
-                                loading ? (
-                                    <div className="d-flex align-items-center">
-                                        <span className='mr-3'>Logging In</span>
-                                        <div className="spinner-border" style={{width: "1.5rem", height: "1.5rem"}} role="status">
-                                            <span className="sr-only">Loading...</span>
-                                        </div>
-                                    </div>
-                                ) : "LOGIN"
-                            }
-                            handleClick={handleLogin}
-                            padding='16'
-                            backgroundColor='gray'
-                            color='white'
-                        />
->>>>>>> 174546e3b286f8ff203e819cedc51ef05706146a
                     </div>
                     <hr />
                     <p className="mt-2 mx-4">Don't have an account? <NavLink className="text-decoration-none" to="/register">Register here</NavLink></p>
