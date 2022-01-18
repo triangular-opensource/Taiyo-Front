6b7beba3b5f62d718a47d6613fab45e4031641e5
savagecarol <karthiksharma1411@gmail.com>
2022-01-18 22:18:38 +0530

    changes in register and contact form

--- src/global/Function.js
+++ src/global/Function.js
@@ -2,21 +2,6 @@ import axios from 'axios';
 import { GLOBAL_URL } from './Constant';
 import  alertMessage from './AlertProvider'
 
-export async function postContact(name, email, subject , message ) 
-{   
-    if(name === "" || subject === "" || message === "")
-        return  alertMessage(<p> Please Fill all the fields</p>);
-
-
-    await axios.post(`${GLOBAL_URL}/contact`, {
-            "name" :  name ,
-            "email" : email ,
-            "subject" : subject ,
-            "message" : message
-        }).then(async (response) => {
-            alertMessage(<p> Thankyou for Contacting Us , We Will Get Back To You</p>);
-    }).catch(async (error) =>  alertMessage(<p> OOps Some Problem Occur </p>) )
-}
 
 
 
